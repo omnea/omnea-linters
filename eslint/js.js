29544268019ea1ef8019d0cad4547dc01741107f
David Bonet <webbonet@gmail.com>
2020-02-06 14:57:00 +0100

    add version for js

---
+++ eslint/js.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "node": true,
+    "es2015": true,
+    "jest": true
+  },
+  "extends": ["eslint:recommended", "plugin:prettier/recommended"],
+  "rules": {
+    "no-unused-vars": 0,
+    "curly": 2,
+  },
+  "settings": {}
+};
